15472ca5d3e6462cc685fa66ec8646dd260c554e
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2024-09-22 14:23:31 +0300

    Finish Days Screen

---
+++ lib/view/days_screen.dart
@@ -0,0 +1,165 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:weatherly/utils/app_color.dart';
+import 'package:weatherly/utils/app_image.dart';
+
+class DaysScreen extends StatelessWidget {
+  const DaysScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+       
+        },
+      ),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: const [
+                AppColor.sColor, // Lighter blue (sky blue)
+                AppColor.pColor, // Darker blue (ocean blue)
+              ],
+              begin: Alignment.center,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Icon(Icons.arrow_back_ios_new, color: Colors.white),
+                    Spacer(flex: 1),
+                    Column(
+                      children: [
+                        Text('Today’s Weather',
+                            style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                        Text(
+                          'Sunday, 8 March 2021',
+                          style: TextStyle(fontSize: 14.0, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Spacer(flex: 1),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: const [AppColor.sColor, AppColor.pColor],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomCenter),
+                    borderRadius: BorderRadius.circular(32),
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SvgPicture.asset(
+                        AppImage.weatherIcon,
+                        height: 150,
+                        width: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            '23°C',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 26.0),
+                          ),
+                          Text(
+                            'Sunny',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 24.0),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(32)),
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return Divider();
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              AppImage.weatherIcon,
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              '23°',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 28.0),
+                            ),
+                            Column(
+                              children: const [
+                                Text(
+                                  'Sunny',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 24.0),
+                                ),
+                                Text(
+                                  'Sunday',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14.0),
+                                )
+                              ],
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
