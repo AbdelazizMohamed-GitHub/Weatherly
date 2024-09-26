3b193debdce8c4851fbd593fea6a84ebe6d6c03b
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2024-09-27 01:08:47 +0300

    Add Location Service And fix Kotlin Error

--- lib/core/service/weather_service.dart
+++ lib/core/service/weather_service.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-
 import 'package:weatherly/model/weather_entity.dart';
 import 'package:weatherly/model/weather_mapper.dart';
 import 'package:weatherly/model/weather_model.dart';
@@ -10,7 +9,7 @@ class WeatherService {
   String apiKey = "8bbac42755bb4f52ab2161355242901";
 
   Future<WeatherEntity> getWeather(String city) async {
-    String url = "$baseUrl/forecast.json?key=$apiKey&q=$city&days=7";
+    String url = "$baseUrl/forecast.json?key=$apiKey&q=$city&days=7&lang=ar";
     var response = await dio.get(url);
     if (response.statusCode == 200) {
       return WeatherMapper.mapModelToEntity(
@@ -20,5 +19,16 @@ class WeatherService {
     }
   }
 
- 
+  Future<WeatherEntity> getWeatherForCurrentLocation( {required double latitude,required double longitude})async {
+
+ String url = "$baseUrl/forecast.json?key=$apiKey&q=$latitude,$longitude&days=7&lang=ar";
+    var response = await dio.get(url);
+    if (response.statusCode == 200) {
+      return WeatherMapper.mapModelToEntity(
+          WeatherModel.fromJson(response.data));
+    } else {
+      throw Exception('Failed to load weather');
+    }
+
+  }
 }
